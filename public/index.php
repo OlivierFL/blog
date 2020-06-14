f3af31edf177bb51d74978bfcbc532b39c6ae5fe
Olivier Floch <o.floch@protonmail.com>
2020-06-14 10:41:31 +0200

    Create project structure

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Router\Router;
+
+require '../vendor/autoload.php';
+
+$router = new Router();
+$router->run();
---
+++ src/Controller/FrontController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+class FrontController
+{
+    public function home()
+    {
+        echo 'Hello World';
+    }
+}
