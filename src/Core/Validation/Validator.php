cdeaceb75f358348d04391aadd5962ff5d9bcf1f
Olivier Floch <o.floch@protonmail.com>
2020-08-31 19:22:15 +0200

    Update email validation to use "filter_var" method

--- src/Core/Validation/Validator.php
+++ src/Core/Validation/Validator.php
@@ -118,7 +118,7 @@ class Validator
     {
         $email = $this->getValue($key);
 
-        if (!preg_match('/(?:[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][\d])|1[\d]{2}|[1-9]?[\d]))\.){3}(?:(2(5[0-5]|[0-4][\d])|1[\d]{2}|[1-9]?[\d])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])/', $email)) {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $this->addError($key, 'email');
 
             return $this;
