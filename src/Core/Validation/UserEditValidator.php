33a9e4c5d2baf2d945522134db2bd049d79c5fe7
Olivier Floch <o.floch@protonmail.com>
2020-09-18 08:53:49 +0200

    Refactor "getValidator" method in "userEditValidator"

--- src/Core/Validation/UserEditValidator.php
+++ src/Core/Validation/UserEditValidator.php
@@ -13,35 +13,45 @@ final class UserEditValidator extends Validator implements ValidatorInterface
     {
         foreach ($this->data as $key => $value) {
             if ($value) {
-                switch ($key) {
-                    case 'user_name':
-                        $this->length('user_name', 3)
-                            ->unique('user_name')
-                        ;
-
-                        break;
-                    case 'last_name':
-                    case 'first_name':
-                        $this->length($key, 1);
-
-                        break;
-                    case 'email':
-                        $this->length('email', 5)
-                            ->email('email')
-                            ->unique('email')
-                        ;
-
-                        break;
-                    case 'role':
-                        $this->role('role');
-
-                        break;
-                    default:
-                        break;
-                }
+                $this->addValidations($key);
             }
         }
 
         return $this;
+    }
+
+    /**
+     * @param int $key
+     *
+     * @throws \Exception
+     */
+    private function addValidations(int $key): void
+    {
+        switch ($key) {
+            case 'user_name':
+                $this->length('user_name', 3)
+                    ->unique('user_name')
+                ;
+
+                break;
+            case 'last_name':
+            case 'first_name':
+                $this->length($key, 1);
+
+                break;
+            case 'email':
+                $this->length('email', 5)
+                    ->email('email')
+                    ->unique('email')
+                ;
+
+                break;
+            case 'role':
+                $this->role('role');
+
+                break;
+            default:
+                break;
+        }
     }
 }
