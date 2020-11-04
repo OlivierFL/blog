71e0a557901ac06846faa417e0f9c31c51ff5c09
Olivier Floch <o.floch@protonmail.com>
2020-11-04 18:18:23 +0100

    Add pagination for posts list

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -93,7 +93,11 @@ class Router
         if ($matches[2]) {
             $action = $matches[2];
         }
-        if (!empty($params) && $params['id']) {
+        if (!empty($params)) {
+            if ($params['page']) {
+                return $controller->{$action}($params['page']);
+            }
+
             return $controller->{$action}($params['id']);
         }
 
--- src/Managers/PostManager.php
+++ src/Managers/PostManager.php
@@ -41,15 +41,39 @@ final class PostManager extends Manager
 
     /**
      * @param null|int $limit
+     * @param null|int $offset
      *
      * @return array
      */
-    public function findAllWithAuthor(int $limit = null): array
+    public function findAllWithAuthor(int $limit = null, int $offset = null): array
     {
         $queryLimit = $limit ? ' LIMIT '.$limit : '';
+
+        if (null !== $offset) {
+            $queryLimit = ' LIMIT '.$offset.', '.$limit;
+        }
 
         $query = $this->db->query('SELECT p.id, p.title, p.content, p.slug, p.cover_img, p.alt_cover_img, p.created_at, p.updated_at, u.first_name, u.last_name FROM post p LEFT JOIN admin a on p.admin_id = a.id LEFT JOIN user u on a.user_id = u.id ORDER BY p.updated_at DESC'.$queryLimit);
 
         return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @param null|int $limit
+     * @param null|int $offset
+     *
+     * @return array
+     */
+    public function findAllWithAuthorPaginated(int $limit = null, int $offset = null): array
+    {
+        $result['data'] = $this->findAllWithAuthor($limit, $offset);
+        $result['total'] = $this->getTotal();
+
+        return $result;
+    }
+
+    private function getTotal()
+    {
+        return $this->db->query('SELECT COUNT(*) FROM post')->fetchAll(PDO::FETCH_ASSOC)[0]['COUNT(*)'];
     }
 }
